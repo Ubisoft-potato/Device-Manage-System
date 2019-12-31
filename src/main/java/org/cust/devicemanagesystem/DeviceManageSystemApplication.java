55309a42f397078f251d34a2389b79108e9cca10
cyka <1354250064mdzz@gmail.com>
2019-12-31 10:53:35 +0800

    init project

---
+++ src/main/java/org/cust/devicemanagesystem/DeviceManageSystemApplication.java
@@ -0,0 +1,13 @@
+package org.cust.devicemanagesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeviceManageSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeviceManageSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/cust/devicemanagesystem/DeviceManageSystemApplicationTests.java
@@ -0,0 +1,16 @@
+package org.cust.devicemanagesystem;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DeviceManageSystemApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
