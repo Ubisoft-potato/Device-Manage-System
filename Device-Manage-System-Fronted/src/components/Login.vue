50154dfc02b67bb05de470812f67dd827718cdd6
ubisoft-potato <1354250064mdzz@gmail.com>
2020-01-02 14:59:30 +0800

    vue+element ui 登录页

--- Device-Manage-System-Fronted/src/components/Login.vue
+++ Device-Manage-System-Fronted/src/components/Login.vue
@@ -1,28 +1,28 @@
 <template>
-  <body class="background">
-  <el-image
-    style="width: 400px; height: 170px"
-    :src="require('../assets/cust.jpeg')"
-    fit="contain"/>
-  <el-form class="login-container" label-position="left"
-           label-width="0px">
-    <h3 class="login_title">系统登录</h3>
-    <el-form-item>
-      <el-input type="text" v-model="loginForm.username"
-                auto-complete="off" placeholder="账号"/>
-    </el-form-item>
-    <el-form-item>
-      <el-input type="password" v-model="loginForm.password"
-                auto-complete="off" placeholder="密码"/>
-    </el-form-item>
-    <el-form-item style="width: 100%">
-      <el-button type="primary" style="width: 100%;background: #0095ce;border: none" @click="login" v-loading="loading">
-        登录
-      </el-button>
-    </el-form-item>
-  </el-form>
-  </body>
-
+  <div>
+    <el-image
+      style="width: 400px; height: 170px"
+      :src="require('../assets/cust.jpeg')"
+      fit="contain"/>
+    <el-form class="login-container" label-position="left"
+             label-width="0px">
+      <h3 class="login_title">系统登录</h3>
+      <el-form-item>
+        <el-input type="text" v-model="loginForm.username"
+                  auto-complete="off" placeholder="账号"/>
+      </el-form-item>
+      <el-form-item>
+        <el-input type="password" v-model="loginForm.password"
+                  auto-complete="off" placeholder="密码"/>
+      </el-form-item>
+      <el-form-item style="width: 100%">
+        <el-button type="primary" style="width: 100%;background: #0095ce;border: none" @click="login"
+                   v-loading="loading">
+          登录
+        </el-button>
+      </el-form-item>
+    </el-form>
+  </div>
 </template>
 
 <script>
