9c9047ecc5afd8720197bb619427a2e48b0099fe
cyka <1354250064mdzz@gmail.com>
2020-01-04 10:35:29 +0800

    删除选中函数

--- Device-Manage-System-Fronted/src/components/admin/Admin.vue
+++ Device-Manage-System-Fronted/src/components/admin/Admin.vue
@@ -2,34 +2,33 @@
   <el-menu
     class="categories"
     default-active="0"
-    @select="handleSelect"
     active-text-color="#409EFF">
     <el-menu-item index="0">
-      <i class="el-icon-menu"></i>
+      <i class="el-icon-menu"/>
       <span slot="title">全部</span>
     </el-menu-item>
     <el-menu-item index="1">
-      <i class="el-icon-menu"></i>
+      <i class="el-icon-menu"/>
       <span slot="title">文学</span>
     </el-menu-item>
     <el-menu-item index="2">
-      <i class="el-icon-menu"></i>
+      <i class="el-icon-menu"/>
       <span slot="title">流行</span>
     </el-menu-item>
     <el-menu-item index="3">
-      <i class="el-icon-menu"></i>
+      <i class="el-icon-menu"/>
       <span slot="title">文化</span>
     </el-menu-item>
     <el-menu-item index="4">
-      <i class="el-icon-menu"></i>
+      <i class="el-icon-menu"/>
       <span slot="title">生活</span>
     </el-menu-item>
     <el-menu-item index="5">
-      <i class="el-icon-menu"></i>
+      <i class="el-icon-menu"/>
       <span slot="title">经管</span>
     </el-menu-item>
     <el-menu-item index="6">
-      <i class="el-icon-menu"></i>
+      <i class="el-icon-menu"/>
       <span slot="title">科技</span>
     </el-menu-item>
   </el-menu>
