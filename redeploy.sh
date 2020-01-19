b6662276f6d35599eb51c9b3e53ae31f4a842d1a
cyka <1354250064mdzz@gmail.com>
2020-01-19 20:13:51 +0800

    docker部署脚本

---
+++ redeploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+docker stop device-manage-system || true
+docker rmi  device-manage-system:dev || true
+cd  Device-Manage-System-Server
+mvn clean package -DskipTests
+docker build -t device-manage-system:dev .
+docker run --rm  \
+--link mysql5.7:mysql  \
+-e ACTIVE_ENV=dev \
+--name device-manage-system \
+-v /etc/localtime:/etc/localtime \
+-v /etc/timezone:/etc/timezone \
+-p 8081:8081 device-manage-system:dev
