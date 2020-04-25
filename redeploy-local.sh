ae3b2192c5d544c6634983c62223960da283f263
cyka <1354250064mdzz@gmail.com>
2020-04-25 19:26:39 +0800

    添加k8s部署脚本

--- redeploy-local.sh
+++ redeploy-local.sh
@@ -4,10 +4,5 @@ docker rmi  device-manage-system:dev || true
 cd  Device-Manage-System-Server
 mvn clean package -DskipTests
 docker build -t device-manage-system:dev .
-docker run --rm  -d \
---link mysql5.7:mysql  \
--e ACTIVE_ENV=dev \
---name device-manage-system \
--v /etc/localtime:/etc/localtime \
--v /etc/timezone:/etc/timezone \
--p 8081:8081 device-manage-system:dev
+kubectl delete -f deployment.yaml | true
+kubectl apply -f deployment.yaml
