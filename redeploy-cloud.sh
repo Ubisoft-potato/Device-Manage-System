7759ae0e96dec0867291e99357e288fe85758c34
cyka <1354250064mdzz@gmail.com>
2020-04-25 19:31:31 +0800

    k8s 环境部署

--- redeploy-cloud.sh
+++ redeploy-cloud.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
-docker stop device-manage-system || true
+kubectl delete -f deployment.yaml | true
 docker rmi  device-manage-system:dev || true
 cd  Device-Manage-System-Server
 mvn clean package -DskipTests
 docker build -t device-manage-system:dev .
-docker run --rm  -d \
---link mysql5.7:mysql  \
--e ACTIVE_ENV=dev \
---name device-manage-system \
--v /etc/localtime:/etc/localtime \
--v /etc/timezone:/etc/timezone \
--p 8081:8081 device-manage-system:dev
+kubectl apply -f deployment.yaml
--- redeploy-local.sh
+++ redeploy-local.sh
@@ -4,5 +4,10 @@ docker rmi  device-manage-system:dev || true
 cd  Device-Manage-System-Server
 mvn clean package -DskipTests
 docker build -t device-manage-system:dev .
-kubectl delete -f deployment.yaml | true
-kubectl apply -f deployment.yaml
+docker run --rm  -d \
+--link mysql5.7:mysql  \
+-e ACTIVE_ENV=dev \
+--name device-manage-system \
+-v /etc/localtime:/etc/localtime \
+-v /etc/timezone:/etc/timezone \
+-p 8081:8081 device-manage-system:dev
