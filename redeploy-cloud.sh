1e6a028fcb1c9bbbc9ffd5d687012799d4347f24
cyka <1354250064mdzz@gmail.com>
2020-04-28 10:53:25 +0800

    脚本修改

--- redeploy-cloud.sh
+++ redeploy-cloud.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
+cd  Device-Manage-System-Server
 kubectl delete -f deployment.yaml | true
 docker rmi  device-manage-system:dev || true
-cd  Device-Manage-System-Server
 mvn clean package -DskipTests
 docker build -t device-manage-system:dev .
 kubectl apply -f deployment.yaml
